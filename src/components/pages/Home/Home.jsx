9db510a0a45075f2d46d1d54a7febfa0abbe1ef7
Hossain K. M. <awarehossain@gmail.com>
2023-10-06 13:59:31 +0600

    5th commit

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 
 function Home({ games }) {
   return (
-    <section id="home" className="home">
+    <section id="home" className="home active">
       <div className="container-fluid">
         <div className="row">
           <GameSwiper games={games} />
